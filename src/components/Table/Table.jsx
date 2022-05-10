9e4a31ebd137985b75b6185bdfb5be4202979076
Alex-Mercy <hambiru@yandex.ru>
2022-05-10 19:45:00 +0300

    sort by asc/desc

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,41 +1,59 @@
 import React from 'react';
+import { useDispatch, } from 'react-redux';
 
 import styles from './Table.module.scss';
 import sortImage from '../../assets/images/sort.png';
 import sortImageByAsc from '../../assets/images/sortByAsc.png';
 import sortImageByDesc from '../../assets/images/sortByDesc.png';
+import { setSortBy, toggleOrder } from '../../store/tableReducer';
+
+function Table({ headers, data, sortBy, isAscOrder }) {
+    const dispatch = useDispatch();
+
+    const changeSortOrder = (e) => {
+        const sortype = headers.find(item => item.name === e.target.innerText).type;
+        dispatch(toggleOrder(!isAscOrder));
+        dispatch(setSortBy(headers.find(item => item.name === e.target.innerText).type));
+    }
+    
+    let imageSort;
+    if (isAscOrder === true) {
+        imageSort = sortImageByAsc;
+    } else {
+        imageSort = sortImageByDesc;
+    }
 
 
-
-function Table({headers, data, sortBy}) {
     return (
         <div>
             <table>
-            <thead>
-                <tr className={styles.tableHeader}>
-                <th>Дата</th>
-                {headers.slice(1).map(header => {
-                    return <th 
-                    className={styles.headerWithFilter} 
-                     key={header.id}>
-                    {header.name}
-                    <img className={styles.sortImage} 
-                    src={header.type === sortBy ? sortImageByAsc : sortImage} 
-                    alt='Sort png' 
-                    />
-                    </th>
-                })}
-                </tr>
+                <thead>
+                    <tr className={styles.tableHeader}>
+                        <th>Дата</th>
+                        {headers.slice(1).map(header => {
+                            return <th
+                                className={styles.headerWithFilter}
+                                key={header.id}
+                                onClick={changeSortOrder}
+                            >
+                                {header.name}
+                                <img className={styles.sortImage}
+                                    src={header.type === sortBy ? imageSort : sortImage}
+                                    alt='Sort png'
+                                />
+                            </th>
+                        })}
+                    </tr>
                 </thead>
                 <tbody>
-                {data.map(item => {
-                    return <tr key={item.id} >
-                        <td className={styles.columns}>{item.date}</td>
-                        <td className={styles.columns}>{item.name}</td>
-                        <td className={styles.columns}>{item.quantity}</td>
-                        <td className={styles.columns}>{item.distance} Км</td>
-                    </tr>
-                })}
+                    {data.map(item => {
+                        return <tr key={item.id} >
+                            <td className={styles.columns}>{item.date}</td>
+                            <td className={styles.columns}>{item.name}</td>
+                            <td className={styles.columns}>{item.quantity}</td>
+                            <td className={styles.columns}>{item.distance} Км</td>
+                        </tr>
+                    })}
                 </tbody>
             </table>
         </div>
