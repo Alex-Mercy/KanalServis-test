dbb24735666dcec0899b60cc26f31d792fbf63e5
Alex-Mercy <hambiru@yandex.ru>
2022-05-12 15:00:26 +0300

    Updates

---
+++ precache-manifest.87be0d913063be41b9a87b8c896d774b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2092be3ddfac7516bf07ce5a8868ee93",
+    "url": "/KanalServis-test/index.html"
+  },
+  {
+    "revision": "ca374f124123ddf511bd",
+    "url": "/KanalServis-test/static/css/main.51363cb9.chunk.css"
+  },
+  {
+    "revision": "271d05927f5ebb174236",
+    "url": "/KanalServis-test/static/js/2.54c4ff7f.chunk.js"
+  },
+  {
+    "revision": "ca374f124123ddf511bd",
+    "url": "/KanalServis-test/static/js/main.23f2e033.chunk.js"
+  },
+  {
+    "revision": "757ed00831fdb9005978",
+    "url": "/KanalServis-test/static/js/runtime-main.1335d703.js"
+  }
+]);
